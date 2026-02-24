c43ef37c09da3c2d62cfc7a140cabec9773dc165
robertgjx <gonzalez.00411@gmail.com>
2026-02-24 12:50:33 -0600

    Update vehicles.ts

--- app/lib/vehicles.ts
+++ app/lib/vehicles.ts
@@ -1,15 +1,22 @@
 export type Vehicle = {
-    id: string;
-    year?: number| null;
-    make: string;
-    model: string;
-    price?: number| null;
-    miles?: number| null;
-    transmission: string;
-    fuel: string;
-    image: string; // ex: "/cars/maroon-chevy.JPG"
-  };
+  id: string;
+  year: number;
+  make: string;
+  model: string;
+
+  price: number | null;
+  miles: number | null;
+
+  // New sheet fields
+  driveTrain: string | null;
+  down: number | null;
+  fuel: string | null;
+
+  // Website fields
+  image: string;
+  status: "available" | "pending" | "sold" | string;
+};
   
   import inventory from "@/data/inventory.json";
 
-  export const vehicles = inventory;
+  export const vehicles = inventory as Vehicle[];
