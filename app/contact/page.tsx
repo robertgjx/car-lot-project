bede12a7cc4245770997415e73afe8a4c4f483aa
Roberto Gonzalez Jr <robertogonzalezjr@MacBookAir.lan>
2026-03-08 23:25:19 -0500

    facebook F

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -56,7 +56,9 @@ export default function ContactPage() {
             rel="noopener noreferrer"
             className="w-full flex items-center justify-center gap-3 rounded-2xl bg-red-600 px-5 py-4 hover:bg-red-700 transition"
           >
-            <Image src="/facebook.png" alt="Facebook" width={24} height={24} />
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/>
+</svg>
             <span className="font-semibold text-white">Garcia&apos;s Auto Sales RGV</span>
           </a>
         </div>
