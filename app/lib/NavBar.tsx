8225c35c5d59ea77be360cb8958d91afb4677c24
Roberto Gonzalez Jr <robertogonzalezjr@MacBookAir.lan>
2026-03-09 22:52:53 -0500

    shrink nav bar

--- app/lib/NavBar.tsx
+++ app/lib/NavBar.tsx
@@ -11,7 +11,7 @@ export default function NavBar() {
     <>
       {/* TOP INFO BAR */}
       <div className="bg-red-600 text-white text-xs">
-        <div className="max-w-6xl mx-auto px-4 md:px-10 py-2 flex flex-wrap items-center justify-between gap-y-1 gap-x-4">
+        <div className="max-w-6xl mx-auto px-4 md:px-10 py-0 flex flex-wrap items-center justify-between gap-y-1 gap-x-4">
           {/* Locations */}
           <div className="flex flex-wrap items-center gap-x-4 gap-y-1">
             <span className="flex items-center gap-1">
